2846a4f15361bacdf8e081b7e7cf1bc33a3da9e4
christopher1121 <45854622+christopher1121@users.noreply.github.com>
2019-10-22 21:01:35 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By iRealKsA");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("636258798100807710")
+setInterval(function() {
+channel.send(`غغَسسسيل أموال من قِبل السَيد المُخضضرم كريسستوفير, `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
